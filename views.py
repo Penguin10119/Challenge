4d8b02cdd4acbc165521cd2ba3898d41dfe2bc79
Penguin10119 <85607253+Penguin10119@users.noreply.github.com>
2021-10-08 13:56:46 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.http import Http404
 
 from polls.models import Question
 
@@ -24,4 +25,21 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = ', '.join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+
 
